3ba633e8f0a70faadb84814e9d07e232e044ba71
Aditya Singh Yadav <aditya@Adityas-MacBook-Air.local>
2026-09-10 01:29:22 +0530

    hi

--- Day26/Q51.c
+++ Day26/Q51.c
@@ -1,1 +1,36 @@
+//Q51: Write a program to print the following pattern:
+   // 5
+   //45
+  //345
+ //2345
+//12345
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+    5
+   45
+  345
+ 2345
+12345
+
+*/
 #include <stdio.h>
+int main(void)
+{   
+     const int N = 5;
+     for (int row = 0; row < N; row++)
+     {
+        for (int i = 0; i < (N - 1 - row); i++)
+        {
+            printf(" ");
+        }
+        for (int j = N - row; j <= N; j++)
+        {
+            printf("%i", j);
+        }
+        printf("\n");
+     }
+}
--- Day26/Q52.c
+++ Day26/Q52.c
@@ -1,1 +1,55 @@
+//Q52: Write a program to print the following pattern:
+
+//*
+
+//*
+//*
+//*
+
+//*
+//*
+//*
+//*
+//*
+
+//*
+//*
+//*
+
+//*
+
+
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with stars spaced irregularly as shown.
+
+*/
 #include <stdio.h>
+int main(void)
+{
+    const int N = 5;
+    int count = 1;
+    while (count <= N)
+    {
+        for  (int i = 0; i < count; i++)
+        {
+            printf("*\n");
+        }
+        printf("\n");
+        count += 2;     
+    }
+    count -= 4;
+    while (count >= 1)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            printf("*\n");
+        }
+        printf("\n");
+        count -= 2;
+    }
+}
