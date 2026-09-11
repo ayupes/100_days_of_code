1371cf27332358f1dff371a68a3b11b58bc4cd04
Aditya Singh Yadav <aditya@Adityas-MacBook-Air.local>
2026-09-12 02:33:52 +0530

    today

--- Day27/Q53.c
+++ Day27/Q53.c
@@ -1,1 +1,49 @@
+//Q53: Write a program to print the following pattern:
+//*
+//***
+//*****
+//*******
+//*********
+//*******
+//*****
+//***
+//*
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+*/
 #include <stdio.h>
+int main(void)
+{
+    const int N = 9;
+    for (int star = 1; star <= N; star += 2)
+    {
+        for (int i = 0; i < star; i++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int star = N - 2; star >= 1; star -= 2)
+    {
+        for (int i = 0; i < star; i++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+}
--- Day27/Q54.c
+++ Day27/Q54.c
@@ -1,1 +1,51 @@
+//Q54: Write a program to print the following pattern:
+
+   //*
+  //***
+ //*****
+//*******
+ //*****
+  //***
+   //*
+
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with layers of stars as shown.
+
+*/
 #include <stdio.h>
+int main(void)
+{
+   const int highest_star_term = 7;
+   for (int row = 0; row < (highest_star_term / 2 + 1); row++)
+   {
+        for (int i = 0; i < (highest_star_term / 2 - row); i++)
+        {
+            printf(" ");
+        }
+        for (int i = 0; i < (2 * row + 1); i++)
+        {
+            printf("*");
+        }
+        printf("\n");
+   }
+   for (int row = 0; row < highest_star_term / 2; row++)
+   {
+        for (int i = 0; i < (row + 1); i++)
+        {
+            printf(" ");
+        }
+        for (int i = 0; i < ((highest_star_term - 2) - 2 * row); i++)
+        {
+            printf("*");
+        }
+        printf("\n");
+   }
+
+   
+    
+}
